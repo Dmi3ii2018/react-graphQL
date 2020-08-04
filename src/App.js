c79bc42e55b551af6e8b8024558111f2422f8b8b
Dmi3ii2018 <dalekseev2017@gmail.com>
2020-08-04 13:00:15 +0500

    Динамическое изменение запросов

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { useQuery } from '@apollo/react-hooks';
 import { loader } from 'graphql.macro';
@@ -6,16 +6,20 @@ import { loader } from 'graphql.macro';
 const query = loader('./GetRegions.graphql');
 
 function App() {
+  const [searchQuery, setSearchQuery] = useState('');
+
   const {data, loading} = useQuery(query, {
-    variables: {searchString: "t"}
+    variables: {searchString: searchQuery}
   });
-  if(loading) {
-    return <p>Loading...</p>
-  }
+
+  const onSearchChange = (evt) => setSearchQuery(evt.target.value);
+
   console.log(data);
   return (
     <div>
       <h1>React Apollo</h1>
+      <input type="text" value={searchQuery} onChange={onSearchChange} />
+      {loading ? <p>Loading...</p> : null}
     </div>
   );
 }
